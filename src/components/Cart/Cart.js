89887440a4378a4ebd332c9fcf09fa2e1226b5ff
sn77sohan <sohan.ahmedself@gmail.com>
2021-11-26 09:05:23 +0600

    third-edition

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Cart.css';
+import Product from './../Products/Product';
 
 const Cart = (props) => {
     const { cart } = props;
-    let total = 0;
-    for (const product of cart ) {
-        total = total + product.price;
-    }
+    // let total = 0;
+    // for (const product of cart ) {
+    // total = total + product.price;
+    // }
+
+    // const total = cart.reduce((previous, product) => previous + product.price, 0)
+
+    const tortalReducer = (previous, product) =>previous + product.price;
+    const total = cart.reduce(tortalReducer, 0);
      return (
         <div>
               <h3>Order Summary</h3>
